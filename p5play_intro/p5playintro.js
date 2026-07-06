e15b608b5111b3e1af745f1e2c4171db9cf6d868
LuLuCheeky <lulucheekypro@gmail.com>
2026-07-06 11:03:37 +0000

    Jul 6, 2026, 11:03 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -32,13 +32,18 @@ function setup() {
     b.w = 40;
     b.h = 40;
     b.color = color(random(255), random(255), random(255));
-    b.vel.x = random(-2, 2);
-    b.vel.y = random(-2, 2);
-    b.collider = "dynamic";
-    b.bounciness = 1.0;
     boxes.push(b);
   }
 
+  box2 = new Sprite();
+  box2.x = 400;
+  box2.y = 200;
+  box2.w = 50;
+  box2.h = 50;
+  box2.color = "yellow";
+  box2.vel.x = 4;
+  box2.vel.y = 3;
+  box2.collider = "dynamic";
 }
 
 function draw() {
@@ -79,25 +84,6 @@ function draw() {
   if (box2.y > height - box2.h / 2) {
     box2.vel.y *= -1;
     box2.y = height - box2.h / 2;
-  }
-
-  for (let b of boxes) {
-    if (b.x < b.w / 2) {
-      b.vel.x *= -1;
-      b.x = b.w / 2;
-    }
-    if (b.x > width - b.w / 2) {
-      b.vel.x *= -1;
-      b.x = width - b.w / 2;
-    }
-    if (b.y < b.h / 2) {
-      b.vel.y *= -1;
-      b.y = b.h / 2;
-    }
-    if (b.y > height - b.h / 2) {
-      b.vel.y *= -1;
-      b.y = height - b.h / 2;
-    }
   }
 
   box.x = mouse.x;
