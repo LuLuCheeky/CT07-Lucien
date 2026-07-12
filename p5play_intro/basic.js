f4ea2948b393b53b9f5c6a6ba5fb4b757ce6ad7f
LuLuCheeky <lulucheekypro@gmail.com>
2026-07-12 08:45:02 +0000

    Jul 12, 2026, 8:45 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -7,10 +7,10 @@ function setup() {
   let area = height * width / 2;
   console.log(area);
   text(area, 100, 100)
-  for (let i = 9; i <= 20; i++) {
+  for (let i = 10; i <= 20; i++) {
     console.log(i);
   }
-  for (let i = 19; i > 1; i--) {
+  for (let i = 20; i > 1; i--) {
     console.log(i);
   }
   for (let i = 3; i < 46; i+=3) {
