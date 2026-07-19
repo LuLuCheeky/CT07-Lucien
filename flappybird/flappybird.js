50a7ba7a4dd1110c6d26ab9837a39cc7e798a0bb
LuLuCheeky <lulucheekypro@gmail.com>
2026-07-19 09:16:34 +0000

    Jul 19, 2026, 9:16 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -15,7 +15,7 @@ function setup() {
     bird.collider = "dynamic";
     bird.mass = 2;
     bird.drag = 0.02;
-    bird.bounciness = 10;
+    bird.bounciness = 5;
     world.gravity.y = 10;
 
     floor = new Sprite();
