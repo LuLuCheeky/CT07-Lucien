4e561f24b3ede4b30f2a38f158acfd07e2454f32
LuLuCheeky <lulucheekypro@gmail.com>
2026-07-19 09:00:44 +0000

    Jul 19, 2026, 9:00 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -13,7 +13,7 @@ function draw() {
 
 
 function preload() {
-    plapMidImg = loadImage("assets/yellowbird-midflap.png");
+    flapMidImg = loadImage("assets/yellowbird-midflap.png");
     bg = loadImage("assets/background-day.png");
     base = loadImage("assets/base.png");
 }
