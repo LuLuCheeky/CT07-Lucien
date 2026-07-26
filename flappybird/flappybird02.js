59c401e5face5790178ce9c1d7d5e07852f3c1c8
LuLuCheeky <lulucheekypro@gmail.com>
2026-07-26 08:54:43 +0000

    Jul 26, 2026, 8:54 AM

--- flappybird/flappybird02.js
+++ flappybird/flappybird02.js
@@ -78,8 +78,6 @@ function spawnPipePair(){
     bottomPipe = new Sprite(400, midY + gap / 2 + 200, 52, 320, "static");
     bottomPipe.img = pipe;
 
-    topPipe = new Sprite()
-
     pipeGroup.add(bottomPipe);
     pipeGroup.layer = 0;
 }
