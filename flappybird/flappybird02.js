64b56c0259a9bf4d22267ec00457f710eca6454f
LuLuCheeky <lulucheekypro@gmail.com>
2026-07-26 08:44:35 +0000

    Jul 26, 2026, 8:44 AM

--- flappybird/flappybird02.js
+++ flappybird/flappybird02.js
@@ -37,10 +37,10 @@ function draw() {
     }
     if (bird.vel.y < 0) {
         bird.img = flapDownImg
-        bird.rotation = bird.vel.y * -20
+        bird.rotation = bird.vel.y * -10
     } else if (bird.vel.y > 0){
         bird.img = flapUpImg 
-        bird.rotation = bird.vel.y * 10
+        bird.rotation = bird.vel.y * 5
     } else if (bird.vel.y == 0){
         bird.img = flapMidImg
         bird.rotation = 0 
