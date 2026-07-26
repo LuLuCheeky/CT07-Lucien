036aff4d037b126155482db8d3410a1a1a2f0789
LuLuCheeky <lulucheekypro@gmail.com>
2026-07-26 08:09:39 +0000

    Jul 26, 2026, 8:09 AM

---
+++ flappybird/flappybird02.js
@@ -0,0 +1,49 @@
+let bird, floor;
+let flapMidImg, bg, base;
+
+
+function setup() {
+    createCanvas(400, 600);
+    background(255);
+    bird = new Sprite();
+    bird.x = width / 2;
+    bird.y = 350;
+    bird.width = 30;
+    bird.height = 30;
+    bird.img = flapMidImg;
+
+    bird.collider = "dynamic";
+    bird.mass = 2;
+    bird.drag = 0.02;
+    bird.bounciness = 0.9;
+    world.gravity.y = 10;
+
+    floor = new Sprite();
+    floor.x = 200;
+    floor.y = height - 20;
+    floor.width = 400;
+    floor.height = 125;
+    floor.collider = "static";
+    floor.img = base;
+}
+
+
+function draw() {    
+    image(bg, 0, 0, width, height);
+
+    if (kb.presses("space") || kb.presses("w") || mouseIsPressed) {
+        bird.vel.y = -3;
+        bird.sleeping = false;
+    }
+    fill("black");
+    textSize(20);
+    text("vel.y: " + bird.vel.y.toFixed(2), 10, 20);
+    text("isMoving: " + bird.isMoving, 10, 40);
+    text("isSleeping: " + bird.sleeping, 10, 60);
+}
+
+function preload() {
+    flapMidImg = loadImage("assets/yellowbird-midflap.png");
+    bg = loadImage("assets/background-day.png");
+    base = loadImage("assets/base.png");
+}
