7208b27c1729c31325f7624b7caa40fadea6d7dd
LuLuCheeky <lulucheekypro@gmail.com>
2026-08-02 08:46:06 +0000

    Aug 2, 2026, 8:46 AM

--- flappybird/flappybird03.js
+++ flappybird/flappybird03.js
@@ -19,7 +19,7 @@ function preload() {
     bg = loadImage('assets/background-day.png');
     base = loadImage('assets/base.png');
 
-    gameOverImg
+    gameOverImg = loadImage("assets/gameO")
 }
 
 function setup() {
