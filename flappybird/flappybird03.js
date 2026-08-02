e14ba3d9290769624d836b08d6dc296b7f1351f3
LuLuCheeky <lulucheekypro@gmail.com>
2026-08-02 08:49:59 +0000

    Aug 2, 2026, 8:49 AM

--- flappybird/flappybird03.js
+++ flappybird/flappybird03.js
@@ -29,8 +29,8 @@ function setup() {
     bird = new Sprite();
     bird.x = width / 2;
     bird.y = 200,
-    bird.width = 30;
-    bird.height = 30;
+    bird.width = 20;
+    bird.height = 20;
     bird.img = flapMidImg; // defined earlier in preload()
 
   // setting bird physics
