2f8d9ec60d38a48db9793acd30f8988e30dfd1d1
LuLuCheeky <lulucheekypro@gmail.com>
2026-08-02 08:47:39 +0000

    Aug 2, 2026, 8:47 AM

--- flappybird/flappybird03.js
+++ flappybird/flappybird03.js
@@ -100,7 +100,8 @@ function draw() {
 
     if (bird.collides(pipeGroup) || bird.collides(floor)){
         gameOverLabel = new Sprite(width / 2, height / 2, 192, 42);
-        gameOverLabel.img = game
+        gameOverLabel.img = gameOverImg;
+        gameOver
     }
 
 
