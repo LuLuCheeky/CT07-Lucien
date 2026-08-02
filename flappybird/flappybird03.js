45bf9766d09303c5b7ff0b7126ee138d8218a16b
LuLuCheeky <lulucheekypro@gmail.com>
2026-08-02 08:40:12 +0000

    Aug 2, 2026, 8:40 AM

--- flappybird/flappybird03.js
+++ flappybird/flappybird03.js
@@ -94,6 +94,10 @@ function draw() {
     text('isMoving: ' + bird.isMoving, 10, 40);
     text('sleeping: ' + bird.sleeping , 10, 60);
 
+    if (bird.collides(pipeGroup) || bird.collides(floor)){
+        noLoop
+    }
+
 
 }
  
