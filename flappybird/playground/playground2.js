54a2cef520747f3a0b2234c381e488cd5fb24c79
LuLuCheeky <lulucheekypro@gmail.com>
2026-08-02 08:22:30 +0000

    Aug 2, 2026, 8:22 AM

---
+++ flappybird/playground/playground2.js
@@ -0,0 +1,105 @@
+let bird, floor; // objects
+let flapMidImg,  bg, base; // images
+let flapUpImg,flapDownImg; // images for flap up and down
+let pipe; // image for pipes
+let topPipe, bottomPipe;
+let pipeGroup; // declare the group for pipe
+
+function preload() {
+    // bird image, background and the floor
+    flapMidImg = loadImage('assets/yellowbird-midflap.png'); 
+    // preload images for flap up and down
+    flapUpImg = loadImage('assets/yellowbird-upflap.png');
+    flapDownImg = loadImage('assets/yellowbird-downflap.png')
+
+    pipe = loadImage('assets/pipe-green.png'); // preload image for pipe
+
+    bg = loadImage('assets/background-day.png');
+    base = loadImage('assets/base.png');
+}
+
+function setup() {
+  new Canvas(400, 600);
+
+  // Bird Sprite construction
+  bird = new Sprite();
+  bird.x = width / 2;
+  bird.y = 200,
+  bird.width = 30;
+  bird.height = 30;
+  bird.img = flapMidImg; // defined earlier in preload()
+
+  // setting bird physics
+  bird.collider = "dynamic"; 
+  bird.mass = 2;         // heavier = stronger pull from gravity
+  bird.drag = 0.02;      // air resistance
+  bird.bounciness = 0.5; // how much it bounces when hitting floor
+  world.gravity.y = 10;
+
+  // Floor to bounce bird
+  floor = new Sprite();
+  floor.x = 200;
+  floor.y = height - 20;
+  floor.width = 400;
+  floor.height = 125;
+  floor.collider = "static"; 
+  floor.img = base;
+
+  pipeGroup = new Group();
+}
+
+function draw() {
+  image(bg, 0, 0, width, height);
+
+  // Apply upward push when space is pressed
+  if (kb.presses('space') || mouse.presses()) {
+    bird.vel.y = -5; // which direction do you think this is?
+    bird.sleeping = false; // wake up if sleeping
+  }
+  
+  // Activity: Change image according to flying action/ falling
+  if (bird.vel.y < -1) {
+    bird.img = flapUpImg; // flying upward
+    bird.rotation = -30; // rotate up
+  } 
+  else if (bird.vel.y > 1) {
+    bird.img = flapDownImg; // falling
+    bird.rotation = 30; // rotate down
+  } 
+  else {
+    bird.img = flapMidImg; // neutral
+    bird.rotation = 0;
+  }
+
+  if (frameCount === 1){
+    spawnPipePair();
+  }
+
+  // Debug info (optional)
+  fill("blue");
+  textSize(14);
+  text('vel.y: ' + bird.vel.y.toFixed(2), 10, 20);
+  text('isMoving: ' + bird.isMoving, 10, 40);
+  text('sleeping: ' + bird.sleeping , 10, 60);
+}
+ 
+
+function spawnPipePair(){
+  // fill in this code later
+    // control the gap and height of the top and bottom pipe
+  let gap = 50;
+  let midY = height / 2;
+
+  // create the top pipe
+  topPipe = new Sprite(400, midY - gap / 2 - 200, 52, 320, 'static');
+  topPipe.img = pipe;
+  topPipe.rotation = 180;
+
+  // create the bottom pipe sprite
+  bottomPipe = new Sprite(400, midY + gap / 2 + 200, 52, 320, 'static');
+  bottomPipe.img = pipe;
+
+  pipeGroup.add(topPipe);
+  pipeGroup.add(bottomPipe);
+  pipeGroup.layer = 0;
+}
