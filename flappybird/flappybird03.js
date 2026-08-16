14056fa038c7a644219faca9b04d902d49ea2ead
LuLuCheeky <lulucheekypro@gmail.com>
2026-08-16 08:15:26 +0000

    Aug 16, 2026, 8:15 AM

--- flappybird/flappybird03.js
+++ flappybird/flappybird03.js
@@ -66,6 +66,9 @@ function setup() {
 
     startScreenLabel = new Sprite(width / 2, height / 2, 50, 50, "none");
     startScreenLabel.img = startScreenImg; 
+
+    scoreDigits = new Group();
+    
 }
 
 function draw() {
