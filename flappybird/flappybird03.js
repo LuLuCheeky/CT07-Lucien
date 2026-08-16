a150ee0dcd78db7a57ca9db742a63c572d03bfe9
LuLuCheeky <lulucheekypro@gmail.com>
2026-08-16 08:20:02 +0000

    Aug 16, 2026, 8:20 AM

--- flappybird/flappybird03.js
+++ flappybird/flappybird03.js
@@ -159,5 +159,6 @@ function drawScore(x, y, score, digitWidth, digitHeight){
     scoreDigits.removeAll();
 
     let scoreStr = str(score);
-    let totalWidth = scoreStr.length * digitW
+    let totalWidth = scoreStr.length * digitWidth;
+    let start 
 }
