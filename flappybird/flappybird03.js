030e26d54714c26df34b0279fe7d9fced256e709
LuLuCheeky <lulucheekypro@gmail.com>
2026-08-16 08:14:20 +0000

    Aug 16, 2026, 8:14 AM

--- flappybird/flappybird03.js
+++ flappybird/flappybird03.js
@@ -11,7 +11,7 @@ let startScreenImg;
 let startGame = false;
 let score = 0;
 let numberImages = [];
-let scoreDigits
+let scoreDigits;
 
 function preload() {
     // bird image, background and the floor
